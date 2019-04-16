52f863b69f0c604f9c1fed611ebd7c65d7e80b45
Emmanuel Chigbo <chigboemmanuel@gmail.com>
2019-04-16 17:24:15 +0100

    Configure middleman-deploy in config.rb

--- config.rb
+++ config.rb
@@ -5,6 +5,16 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :deploy do |deploy|
+  deploy.build_before = false # default: false
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -40,7 +50,12 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+
+  # Append a hash to asset urls (make sure to use the url helpers)
+  activate :asset_hash
+
+  # activate :asset_host, :host => '//YOURDOMAIN.cloudfront.net'
+end
